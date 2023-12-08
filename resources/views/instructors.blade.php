6437353e4e007c0ac16a92b9921568a24e4d7099
4KRO5 <4kro54k@gmail.com>
2023-12-08 00:05:04 -0700

    view instructors

--- resources/views/instructors.blade.php
+++ resources/views/instructors.blade.php
@@ -3,6 +3,79 @@
 <head>
     <title>CRUD de Instructores</title>
     <style>
+        body {
+            font-family: 'Roboto', sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+        }
+
+        .action-buttons {
+            display: flex;
+            gap: 10px;
+            align-items: center; /* Align items vertically in the center */
+        }
+
+        .action-buttons button {
+            padding: 8px;
+            border: none;
+            cursor: pointer;
+            border-radius: 4px;
+            margin-bottom: 0; /* Remove the margin-bottom */
+        }
+
+        #container {
+            width: 80%;
+            background-color: #fff;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            overflow: auto; /* Change from 'hidden' to 'auto' */
+            margin: 20px;
+            padding: 10px;
+        }
+
+        h1 {
+            background-color: #333;
+            color: #fff;
+            padding: 20px;
+            margin: 0;
+        }
+
+        button {
+            background-color: #4caf50;
+            color: white;
+            padding: 10px;
+            border: none;
+            cursor: pointer;
+            border-radius: 4px;
+            margin-bottom: 10px;
+        }
+
+        table {
+            width: calc(100% - 20px); /* Establecer la anchura de la tabla al 100% del contenedor */
+            border-collapse: collapse;
+            margin: 10px; /* Reducir el margen de la tabla */
+        }
+
+        th, td {
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding: 8px;
+        }
+
+        th {
+            background-color: #f2f2f2;
+        }
+
+        .deleted-row {
+            opacity: 0.5;
+        }
+
         #popup {
             display: none;
             position: fixed;
@@ -12,11 +85,49 @@
             padding: 20px;
             background: white;
             border: 1px solid black;
-            z-index: 1000; /* Asegúrate de que el popup se muestre por encima de otros elementos */
+        }
+
+        #popupTitle {
+            background-color: #333;
+            color: #fff;
+            padding: 10px;
+            margin: 0;
+            border-radius: 4px 4px 0 0;
+        }
+
+        #paymentForm {
+            padding: 20px;
+        }
+
+        select, input {
+            width: 100%;
+            padding: 8px;
+            margin-bottom: 10px;
+            box-sizing: border-box;
+        }
+
+        #submitButton {
+            background-color: #4caf50;
+            color: white;
+            padding: 10px;
+            border: none;
+            cursor: pointer;
+            border-radius: 4px;
+        }
+
+        #popup button {
+            background-color: #ccc;
+            color: #333;
+            padding: 10px;
+            border: none;
+            cursor: pointer;
+            border-radius: 4px;
+            margin-top: 10px;
         }
     </style>
 </head>
 <body>
+<div id="container">
     <h1>Registro de Instructores</h1>
     <button id="crearButton">Crear Instructor</button>
     <button onclick="toggleInstructors()">
@@ -68,6 +179,7 @@
         </form>
         <button onclick="document.getElementById('popup').style.display='none'">Cerrar</button>
     </div>
+</div>
 
     <script>
         function toggleInstructors() {
